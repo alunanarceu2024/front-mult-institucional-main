c1884e0391b970cf4272adda2ac46b79201264c4
alunanarceu2024 <mariana.lsilva7@aluno.edu.es.gov.br>
2024-11-07 11:32:16 -0300

    feat: update

--- src/components/MainHome/index.tsx
+++ src/components/MainHome/index.tsx
@@ -5,6 +5,7 @@ import './style.css'
 const MainHome: React.FC = () => {
   return (
     <Main>
+      <h1>Conheça nossos produtos:</h1>
       <h1 className="h1-home">Bem-vindo ao universo do chocolate, onde cada pedaço é uma deliciosa viagem</h1>
       <p className="p-home">Oferecemos chocolates artesanais feitos com ingredientes de alta qualidade e técnicas refinadas, transformando cada pedaço em uma experiência de sabor e prazer únicos.ya</p>
       <button className="cta-button">Faça o seu pedido</button>
